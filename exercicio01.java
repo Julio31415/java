2b6e2f13878fb230055306f2fd5feae56e6132f4
Julio Cesar <110460790+Julio31415@users.noreply.github.com>
2024-02-27 20:06:13 -0300

    Iniciando o curso pelo livro Java: Aprenda a programar 10 edição

---
+++ exercicio01.java
@@ -0,0 +1,8 @@
+// Programa de impressão de texto.
+public class exercicio01 {
+
+    //método manin inicia a execução do aplicativo Java
+    public static void main(String[] args){
+        System.out.println("Olá mundo");
+    }//fim do método main
+}//fim da classe exercicio01
